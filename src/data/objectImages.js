c62b9a3afc9161cb28812673375c5557aa7791d5
Thaviru-Stanley <thavirustanley@gmail.com>
2026-03-16 15:02:31 +0530

    10952881

---
+++ src/data/objectImages.js
@@ -0,0 +1,28 @@
+const objectImages = [
+    { label: "Bed 1 Blue", type: "bed1blue", image: "/src/assets/2d/Bed/bed1blue.png" },
+    { label: "Bed 1 Red", type: "bed1red", image: "/src/assets/2d/Bed/bed1red.png" },
+    { label: "Bed 1 White", type: "bed1white", image: "/src/assets/2d/Bed/bed1white.png" },
+    { label: "Bed 2 Blue", type: "bed2blue", image: "/src/assets/2d/Bed/bed2blue.png" },
+    { label: "Bed 2 Red", type: "bed2red", image: "/src/assets/2d/Bed/bed2red.png" },
+    { label: "Bed 2 White", type: "bed2white", image: "/src/assets/2d/Bed/bed2white.png" },
+  
+    { label: "Chair 1 Blue", type: "chair1blue", image: "/src/assets/2d/Chair/chair1blue.png" },
+    { label: "Chair 1 Brown", type: "chair1brown", image: "/src/assets/2d/Chair/chair1brown.png" },
+    { label: "Chair 1 Red", type: "chair1red", image: "/src/assets/2d/Chair/chair1red.png" },
+    { label: "Chair 1 White", type: "chair1white", image: "/src/assets/2d/Chair/chair1white.png" },
+  
+    { label: "Sofa 1 Blue", type: "sofa1blue", image: "/src/assets/2d/Sofa/Sofa 1/sofa1blue.png" },
+    { label: "Sofa 1 Brown", type: "sofa1brown", image: "/src/assets/2d/Sofa/Sofa 1/sofa1brown.png" },
+    { label: "Sofa 1 White", type: "sofa1white", image: "/src/assets/2d/Sofa/Sofa 1/sofa1white.png" },
+  
+    { label: "Sofa 2 Blue", type: "sofa2blue", image: "/src/assets/2d/Sofa/Sofa 2/sofa2blue.png" },
+    { label: "Sofa 2 Brown", type: "sofa2brown", image: "/src/assets/2d/Sofa/Sofa 2/sofa2brown.png" },
+    { label: "Sofa 2 Gray", type: "sofa2gray", image: "/src/assets/2d/Sofa/Sofa 2/sofa2gray.png" },
+    { label: "Sofa 2 Green", type: "sofa2green", image: "/src/assets/2d/Sofa/Sofa 2/sofa2green.png" },
+  
+    { label: "Table Blue", type: "tablebl", image: "/src/assets/2d/Table/table1bl.png" },
+    { label: "Table Brown", type: "tablebr", image: "/src/assets/2d/Table/table1br.png" },
+    { label: "Table White", type: "tablew", image: "/src/assets/2d/Table/table1w.png" }
+  ];
+  
+  export default objectImages;
